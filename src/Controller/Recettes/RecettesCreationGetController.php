33de4d18603f700da2a61414fb1b7fe3ef8d9c09
Billy VILLENA <coding@devrendal.fr>
2025-12-16 18:00:08 +0100

    ✨ [Recettes] Création d'une recette
    * Découverte des formulaires

---
+++ src/Controller/Recettes/RecettesCreationGetController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Recettes;
+
+use App\Entity\Recette;
+use App\Form\RecetteType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route(
+    path: '/recettes/création',
+    name: 'app_recettes_creation_get',
+)]
+class RecettesCreationGetController extends AbstractController
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+    ) {}
+
+    public function __invoke(Request $request): Response
+    {
+        $recette = new Recette();
+        $form = $this->createForm(RecetteType::class, $recette);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($recette);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_recettes_liste_get');
+        }
+
+        return $this->render(
+            view: 'pages/recettes/creation/form.html.twig',
+            parameters: [
+                'form' => $form,
+            ]
+        );
+    }
+}
